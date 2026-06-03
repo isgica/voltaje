27c5acaa0762bf4068ed34f285edcc37f4a77e22
e <e@moderatelygood.software>
2026-06-03 14:45:53 -0400

    hi

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print('hi')
+import streamlit as st
+
+# This will actually show up on the web page!
+st.title("🎈 My First Streamlit App")
+
+st.write("Hello, world! If you can see this, Streamlit is working perfectly.")
+
+# An interactive widget just to test functionality
+if st.button("Click Me"):
+    st.success("It works! 🎉")
+
